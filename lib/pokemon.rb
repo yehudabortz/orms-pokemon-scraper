4b4593e462bd4524e3277ce68f6e5b93ada41426
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-16 14:53:21 -0600

    commit tree e4422c307a42a26fee09d511b3e47a84e5f1fea3

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?) 
+        SQL
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+
+    end
+
+    def self.find(id_num, db)
+        sql = <<-SQL
+            SELECT * 
+            FROM pokemon
+            WHERE pokemon.id = ?
+        SQL
+        result = db.execute(sql, id_num)[0]
+        Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+
+    end
+
+
 end
